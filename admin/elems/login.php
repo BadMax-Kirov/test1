01242a56000bc17b7e3b947bf82229e4c1749ee4
BadMax-Kirov <62816343+BadMax-Kirov@users.noreply.github.com>
2021-02-12 08:29:53 +0300

    Макс
    Пример с админкой

--- admin/elems/login.php
+++ admin/elems/login.php
@@ -1,25 +1,29 @@
+﻿
 <?php
+ 
     if(!empty($_POST['log']) AND !empty($_POST['pass'])){
        $log = $_POST['log'];
        $pass = $_POST['pass'];
        
-       $query = "SELECT * FROM users WHERE login = '$log' AND password = '$pass'";
+       $query = "SELECT * FROM users WHERE login = '$log' AND password = '$pass'"
+              or die ('ERROR!');
        $result = mysqli_query($link, $query);
        
        $user = mysqli_fetch_assoc($result);
-       
        if(!empty($user))
        {
            $_SESSION['auth'] = true;
            header ('location: \admin');
+       }else{
+           header('location: \admin');
        }
     }else {
 ?>
         <form action="" method="POST">
             <label>login</label>
-            <input  name="login" value="<?php $log ?>"><br>
+            <input  name="log" id="log" value=""><br>
             <label>password</label>
-            <input type="password" name="pass" value="<?php $pass ?>"><br>
+            <input type="password" name="pass" id ="pass" value=""><br>
             <input type="submit" value="отправить данные">
         </form>
-<?php var_dump($_POST);} 
+<?php } 
